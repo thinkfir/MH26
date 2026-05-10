200fb17da455f98bcc24e090ab40fa21d7bb82d5
Justin Li <Justinnova0915@gmail.com>
2026-05-10 07:41:48 -0400

    I gen don know what I'm doing

--- src/QueueManager.hpp
+++ src/QueueManager.hpp
@@ -5,18 +5,25 @@
 namespace pp {
 
 class QueueManager : public QObject {
+    Q_Object
 private:
     AudioEngine* audio = new AudioEngine(this);
-    vector<string> queue;
+    std::vector<std::string> queue;
     int cur = 0;
 public:
 
+    void AddTrack(string path) {
+        queue.push_back(path);
+    }
+
     void NextTrack() {
         audio->loadPath(cur++)
+        audio->play()
     }
 
     void PreviousTrack() {
         audio->loadPath(cur--)
+        audio->play()
     }
 
 }
