a494010cd1425fd86c08058238b2b93b06553b59
Arya Aithal <aryaaithal@gmail.com>
2026-05-10 09:52:40 -0400

    Include env.hpp and load environment file
    Added functionality to load environment variables from a .env file at startup.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include "Project.hpp"
 #include "main_class.hpp"
+#include "env.hpp"
 
 // 800x480
 
 int main(int argc, char* argv[]) {
 	std::cout << "PiPod starting...\n";
-
+	loadEnvFile(".env");
 	QApplication app(argc, argv);
 
 	pp::Main window;
